010d387059dd5af23510b2577488a6ec1aeec8ae
Sara Isabel del Sastre <chinchimilla@gmail.com>
2025-11-12 20:04:18 +0100

    Rick&Morty lo que he podido

--- js/script.js
+++ js/script.js
@@ -1,35 +1,16 @@
-/*const listaCaracteres = document.getElementById("character-list");
-const urlBase = 
 
-function traerPersonajes() {
-    fetch(`https://rickandmortyapi.com/api/character/?page=(1)`)
-    .then(response => response.json())
-    .then(data => {
-        const card = `<div class="card"><img src="`https://rickandmortyapi.com/api/character
-    })
-}
-console.log(traerPersonajes);
-
-    /*.then(response => response.json())
-    .then(data)
-    }
-}*/
 const listaCaracteres = document.getElementById("character-list");
 let paginaActual = 1;
 
 function traerPersonajes() {
-  // 1. PEDIR (fetch)
   fetch(`https://rickandmortyapi.com/api/character/?page=${paginaActual}`)
-    // 2. RECIBIR y convertir a JavaScript (response.json())
     .then(response => response.json())
-    // 3. USAR los datos
     .then(data => {
-      console.log(data);  // ← AQUÍ VES QUÉ TRAE LA API
+      console.log(data);
       
-      // Limpia lo viejo
       listaCaracteres.innerHTML = "";
       
-      // Crea cards para cada personaje
+      
       data.results.forEach(personaje => {
         const card = `
           <div class="card">
@@ -38,10 +19,10 @@ function traerPersonajes() {
             <p><b>Species:</b> ${personaje.species}</p>
           </div>
         `;
-        listaCaracteres.innerHTML += card;  // Agrega el card
+        listaCaracteres.innerHTML += card; 
       });
     });
 }
 
-// Llama a la función
+
 traerPersonajes();
