a4502cdced40588e198e3a35bea775c3ca446eea
Sara Isabel del Sastre <chinchimilla@gmail.com>
2025-11-17 09:04:38 +0100

    Rick&Morty corregido

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 const listaCaracteres = document.getElementById("character-list");
 let paginaActual = 1;
 const botonNext = document.getElementById("next-page");
@@ -6,24 +5,44 @@ const botonPrev = document.getElementById("prev-page");
 
 function traerPersonajes() {
   fetch(`https://rickandmortyapi.com/api/character/?page=${paginaActual}`)
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Error en la petición: ${response.status}`);
+      }
+      return response.json();
+    })
     .then(data => {
-      console.log(data);
-      
-      listaCaracteres.innerHTML = "";
-      
-      
-      data.results.forEach(personaje => {
-        const card = `
-          <div class="card">
-            <img src="${personaje.image}" alt="${personaje.image}">
-            <p><strong>Name:</strong> ${personaje.name}</p>
-            <p><b>Species:</b> ${personaje.species}</p>
-          </div>
-        `;
-        listaCaracteres.innerHTML += card; 
-      });
+      const characters = data.results
+        .map(character => {
+          return `
+            <li class="card">
+              <img src="${character.image}" alt="${character.name}" />
+              <h2><strong>Nombre:</strong> ${character.name}</h2>
+              <p><strong>Especie:</strong> ${character.species}</p>
+            </li>
+          `;
+        })
+        .join("");
+
+      listaCaracteres.innerHTML = characters;
+    })
+    .catch(err => {
+      listaCaracteres.innerHTML = `<h3 class="error">${err}</h3>`;
     });
 }
+
+botonPrev.addEventListener("click", () => {
+  if (paginaActual > 1) {
+    paginaActual--;
+    traerPersonajes();
+  }
+});
+
+botonNext.addEventListener("click", () => {
+  if (paginaActual < 42) {
+    paginaActual++;
+    traerPersonajes();
+  }
+});
 
 traerPersonajes();
